c3c43b89f87e200b0323c8645ad4795b4c89166a
Teague Hannam <teaguehannam@gmail.com>
2022-07-09 19:51:11 -0600

    Simple iframe

--- media/src/App.svelte
+++ media/src/App.svelte
@@ -1,38 +1,8 @@
-<script>
-  const playlist = new String('https://www.youtube.com/embed?listType=playlist&list=PL5WBz5u6UE9IMTzbFCnCn9EbvVNHxfHWj')
-  let innerWidth = new Number(window.innerWidth || 0)
-  let emojis = selectEmojis()
-  let textbox // html textrea
 
-  function selectEmojis() {
-    let randomize = [
-      '🤖','🦆','🌵',
-      '🤔','🤤','😷',
-      '😎','👻','👽',
-      '👇','🤘','✌',
-      '😹','💀','💦'].sort(() => 0.5 - Math.random());
-    randomize.unshift('🧙‍♂️'); // always have wizard
-    return new String(randomize.slice(0, 3)).replace(/,/g, '');
-  }
-</script>
-
-
-<svelte:window bind:innerWidth />
-<header on:click={() => emojis = selectEmojis()}>
-    <h1 class="NoSelect">
-      {new String().padStart((innerWidth * 0.04142), emojis)}
-    </h1>
-</header>
-
-<iframe src={playlist}
+<iframe src={'https://www.youtube.com/embed?listType=playlist&list=PL5WBz5u6UE9IMTzbFCnCn9EbvVNHxfHWj'}
   title="YouTube video"
   frameborder="0"
   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
   allowfullscreen
   referrerpolicy="no-referrer-when-downgrade" >
 </iframe>
-
-<footer>
-  <h3 title="YouTube">Teague Hannam</h3>
-  <h3 title="Playlist">music020</h3>
-</footer>
