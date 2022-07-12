ae0e0d11223e44e7c31875c236053b5d2c84a556
Teague Hannam <teaguehannam@gmail.com>
2022-07-12 05:25:54 -0600

    Update 0.6

--- linktree/src/App.svelte
+++ linktree/src/App.svelte
@@ -1,48 +1,45 @@
 <script>
-  const Links = [
-    {
-      "name": "Github",
-      "link": "https://github.com/teaguehannam"
-    },
-    {
-      "name": "Media",
-      "link": "https://media.teaguehannam.com/"
-    },
-    {
-      "name": "Notes",
-      "link": "https://notes.teaguehannam.com/"
-    },
-    {
-      "name": "Nutrients",
-      "link": "https://currentnutrients.com/"
-    },
-    {
-      "name": "YouTube",
-      "link": "https://www.youtube.com/channel/UCZOwiWmYnooVVsVAYmewOhg/playlists"
-    },
-  ]
+  import { links } from './Links.js';
 </script>
 
 <h1>Teague Hannam</h1>
-{#each Links as i}
-  <a href={i.link}>{i.name}</a>
-{/each}
+
+<main>
+  {#each links as i}
+    <a href={i.link}>{i.name}</a>
+  {/each}
+</main>
+
 
 <style>
   h1 {
-    text-align: right;
-    line-height: calc(1rem + 3vw);
+    position: fixed;
+    bottom: 0;
+    right: 0;
     margin: 0;
-    font-size: calc(1rem + 3vw);
+    line-height: 1.8rem;
+    color: rgba(192,192,192,0.15);
+    letter-spacing: 0.1vw;
+    font-weight: 100;
+    font-size: 1.2rem;
+    text-align: center;
+    padding: 0 2vw;
+    background: rgba(0,0,0,0.2);
+  }
+  main {
+    display: grid;
+    align-items: center;
   }
   a {
     display: block;
     text-align: center;
-    font-size: calc(1rem + 3vw);
-    line-height: 18vh;
-    width: 100%;
+    font-size: calc(1.8rem + 1vw);
+    line-height: calc(100vh / 5 - (1.8rem  / 5));
+    transition: 0.12s all ease-out;
   }
-  a:hover {
+  a:hover, a:active, a:focus {
+    backdrop-filter: blur(7px);
+    background: rgba(20,20,20,0.6);
     color: white;
   }
 </style>
---
+++ linktree/src/Links.js
@@ -0,0 +1,23 @@
+
+export const links = [
+  {
+    "name": "Github",
+    "link": "https://github.com/teaguehannam"
+  },
+  {
+    "name": "Media",
+    "link": "https://media.teaguehannam.com/"
+  },
+  {
+    "name": "Notes",
+    "link": "https://notes.teaguehannam.com/"
+  },
+  {
+    "name": "Nutrients",
+    "link": "https://nutrientinfo.app/"
+  },
+  {
+    "name": "YouTube",
+    "link": "https://www.youtube.com/channel/UCZOwiWmYnooVVsVAYmewOhg/playlists"
+  }
+];
