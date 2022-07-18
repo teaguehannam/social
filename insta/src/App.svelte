fdfc0825e69b3059d2b83ea8dfd4ef86bcfea02e
Teague Hannam <teaguehannam@gmail.com>
2022-07-18 05:03:48 -0600

    added insta 📸

---
+++ insta/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let tech: string = "Vite + Svelte"
+</script>
+
+<main>
+  <h1>{tech}</h1>
+  <a href="https://vitejs.dev" target="_blank"> 
+    <img src="/vite.svg" class="logo" alt="Vite Logo" />
+  </a>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    width: 900px;
+    margin: auto;
+  }
+</style>
---
+++ insta/svelte.config.js
@@ -0,0 +1,6 @@
+import sveltePreprocess from 'svelte-preprocess'
+
+export default {
+  // ¿ sass ?
+  preprocess: sveltePreprocess()
+}
